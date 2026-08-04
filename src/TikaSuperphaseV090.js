098a69616287fbcd72e2e782429a0cc6d3cae65c
Michael Hughes <microneesia79@gmail.com>
2026-08-04 12:49:52 -0700

    Add Tika Superphase v0.9 entrypoint

---
+++ src/TikaSuperphaseV090.js
@@ -0,0 +1,7 @@
+import './v090/guideLayer.js';
+import './v090/capabilityManifest.js';
+import './v090/contextBroker.js';
+import './v090/commandRegistry.js';
+import './v090/tikaUI.js';
+
+document.documentElement.dataset.superphase = 'v0.9.0';
